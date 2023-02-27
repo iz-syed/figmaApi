178ee61c78cb3cd9279ca49033daa89d0d3933ef
iz-syed <mubarakcc7@gmail.com>
2023-02-27 18:28:24 +0530

    inint

--- build/ios-swift/StyleDictionary+Enum.swift
+++ build/ios-swift/StyleDictionary+Enum.swift
@@ -4,12 +4,44 @@
 //
 
 // Do not edit directly
-// Generated on Fri, 24 Feb 2023 20:24:35 GMT
+// Generated on Mon, 27 Feb 2023 09:38:01 GMT
 
 
 import UIKit
 
 public enum StyleDictionaryEnum {
+    public static let appBase = [object Object]
+    public static let appDanger = #dc3545
+    public static let appDark = #212529
+    public static let appFontBase = 1rem
+    public static let appFontLarge = 1.25rem
+    public static let appFontSmall = 0.875rem
+    public static let appLight = #f8f9fa
+    public static let appOpacity0 = 0
+    public static let appOpacity1 = 0.1
+    public static let appOpacity2 = 0.2
+    public static let appOpacity3 = 0.3
+    public static let appOpacity4 = 0.4
+    public static let appOpacity5 = 0.5
+    public static let appOpacity6 = 0.6
+    public static let appOpacity7 = 0.7
+    public static let appOpacity8 = 0.8
+    public static let appOpacity9 = 0.9
+    public static let appPrimary = #F9A707
+    public static let appRounded = 0.375rem
+    public static let appRounded2xl = 2rem
+    public static let appRoundedLg = 0.5rem
+    public static let appRoundedPill = 50rem
+    public static let appRoundedSm = 0.25rem
+    public static let appRoundedXl = 1rem
+    public static let appSecondary = #6c757d
+    public static let appSpace1 = 0.25rem
+    public static let appSpace2 = 0.5rem
+    public static let appSpace3 = 1rem
+    public static let appSpace4 = 1.5rem
+    public static let appSpace5 = 3rem
+    public static let appSuccess = #198754
+    public static let appWhite = #ffffff
     public static let colorBaseGrayDark = UIColor(red: 0.067, green: 0.067, blue: 0.067, alpha: 1)
     public static let colorBaseGrayLight = UIColor(red: 0.800, green: 0.800, blue: 0.800, alpha: 1)
     public static let colorBaseGrayMedium = UIColor(red: 0.600, green: 0.600, blue: 0.600, alpha: 1)
@@ -18,18 +50,6 @@ public enum StyleDictionaryEnum {
     public static let colorFontBase = UIColor(red: 1.000, green: 0.000, blue: 0.000, alpha: 1)
     public static let colorFontSecondary = UIColor(red: 0.000, green: 1.000, blue: 0.000, alpha: 1)
     public static let colorFontTertiary = UIColor(red: 0.800, green: 0.800, blue: 0.800, alpha: 1)
-    public static let globalDanger = #dc3545
-    public static let globalDark = #212529
-    public static let globalLight = #f8f9fa
-    public static let globalPrimary = #0d6efd
-    public static let globalSecondary = #6c757d
-    public static let globalSpace1 = 0.25rem
-    public static let globalSpace2 = 0.5rem
-    public static let globalSpace3 = 1rem
-    public static let globalSpace4 = 1.5rem
-    public static let globalSpace5 = 3rem
-    public static let globalSuccess = #198754
-    public static let globalWhite = #ffffff
     public static let sizeFontBase = CGFloat(16.00) /* the base size of the font */
     public static let sizeFontLarge = CGFloat(32.00) /* the large size of the font */
     public static let sizeFontMedium = CGFloat(16.00) /* the medium size of the font */
--- build/ios-swift/StyleDictionary+Struct.swift
+++ build/ios-swift/StyleDictionary+Struct.swift
@@ -4,12 +4,44 @@
 //
 
 // Do not edit directly
-// Generated on Fri, 24 Feb 2023 20:24:35 GMT
+// Generated on Mon, 27 Feb 2023 09:38:01 GMT
 
 
 import UIKit
 
 internal struct StyleDictionaryStruct {
+    internal static let appBase = [object Object]
+    internal static let appDanger = #dc3545
+    internal static let appDark = #212529
+    internal static let appFontBase = 1rem
+    internal static let appFontLarge = 1.25rem
+    internal static let appFontSmall = 0.875rem
+    internal static let appLight = #f8f9fa
+    internal static let appOpacity0 = 0
+    internal static let appOpacity1 = 0.1
+    internal static let appOpacity2 = 0.2
+    internal static let appOpacity3 = 0.3
+    internal static let appOpacity4 = 0.4
+    internal static let appOpacity5 = 0.5
+    internal static let appOpacity6 = 0.6
+    internal static let appOpacity7 = 0.7
+    internal static let appOpacity8 = 0.8
+    internal static let appOpacity9 = 0.9
+    internal static let appPrimary = #F9A707
+    internal static let appRounded = 0.375rem
+    internal static let appRounded2xl = 2rem
+    internal static let appRoundedLg = 0.5rem
+    internal static let appRoundedPill = 50rem
+    internal static let appRoundedSm = 0.25rem
+    internal static let appRoundedXl = 1rem
+    internal static let appSecondary = #6c757d
+    internal static let appSpace1 = 0.25rem
+    internal static let appSpace2 = 0.5rem
+    internal static let appSpace3 = 1rem
+    internal static let appSpace4 = 1.5rem
+    internal static let appSpace5 = 3rem
+    internal static let appSuccess = #198754
+    internal static let appWhite = #ffffff
     internal static let colorBaseGrayDark = UIColor(red: 0.067, green: 0.067, blue: 0.067, alpha: 1)
     internal static let colorBaseGrayLight = UIColor(red: 0.800, green: 0.800, blue: 0.800, alpha: 1)
     internal static let colorBaseGrayMedium = UIColor(red: 0.600, green: 0.600, blue: 0.600, alpha: 1)
@@ -18,18 +50,6 @@ internal struct StyleDictionaryStruct {
     internal static let colorFontBase = UIColor(red: 1.000, green: 0.000, blue: 0.000, alpha: 1)
     internal static let colorFontSecondary = UIColor(red: 0.000, green: 1.000, blue: 0.000, alpha: 1)
     internal static let colorFontTertiary = UIColor(red: 0.800, green: 0.800, blue: 0.800, alpha: 1)
-    internal static let globalDanger = #dc3545
-    internal static let globalDark = #212529
-    internal static let globalLight = #f8f9fa
-    internal static let globalPrimary = #0d6efd
-    internal static let globalSecondary = #6c757d
-    internal static let globalSpace1 = 0.25rem
-    internal static let globalSpace2 = 0.5rem
-    internal static let globalSpace3 = 1rem
-    internal static let globalSpace4 = 1.5rem
-    internal static let globalSpace5 = 3rem
-    internal static let globalSuccess = #198754
-    internal static let globalWhite = #ffffff
     internal static let sizeFontBase = CGFloat(16.00) /* the base size of the font */
     internal static let sizeFontLarge = CGFloat(32.00) /* the large size of the font */
     internal static let sizeFontMedium = CGFloat(16.00) /* the medium size of the font */
